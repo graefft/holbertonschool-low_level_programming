1aadb42b895075e1fabf10513a9e626ccf238b8b
Thomas Graeff <676@holbertonschool.com>
2019-02-06 23:31:08 +0000

    Update 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  *main - print string using puts
  *
- *return: 0
+ *Return: return 0
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
