c2aac3b7a00e19b26423bbf87bb02e0c99c683b5
Thomas Graeff <676@holbertonschool.com>
2019-02-07 23:43:17 +0000

    Add 5.0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - print all single digit numbers 0-9
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
