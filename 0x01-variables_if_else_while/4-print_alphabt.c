60e3dcf7fbca0688183ee9baee6a36bc36c042bf
Thomas Graeff <676@holbertonschool.com>
2019-02-07 23:24:04 +0000

    Add 4.0

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - print alphabet except q and e
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int x = 'a';
+
+	while (x <= 'z')
+	{
+		if (x != 'q' && x != 'e')
+		{
+			putchar(x);
+			x++;
+		}
+	}
+	putchar(x);
+	return (0);
+}
