f18b3896caab7ed7608d4e9583fe5c97e2af63e0
Thomas Graeff <676@holbertonschool.com>
2019-02-07 23:49:05 +0000

    Add 5.2

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,13 +8,13 @@
 
 int main(void)
 {
-	char x = '0';
+	int x = 0;
 
-	while (x < '10')
+	while (x < 10)
 	{
-		putchar(x);
+		printf("%d", x);
 		x++;
 	}
-	putchar('\n');
+	printf("\n");
 	return (0);
 }
