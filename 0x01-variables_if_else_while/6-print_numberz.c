4064d566dd4012dd329dd01c3299e2cdc187f38f
Thomas Graeff <676@holbertonschool.com>
2019-02-07 23:53:01 +0000

    Add 6.0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+/**
+ *main - print all single digit numbers 0-9 with putchar
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	char x = '0';
+
+	while (x < 10)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
