ec2522aad587d41628e19eec268483b3b59e3c61
Thomas Graeff <676@holbertonschool.com>
2019-02-08 00:25:19 +0000

    Add 9.0

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit numbers with commas using putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		putchar(x + '0');
+		if (x > 8)
+		{
+			putchar('\n');
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
