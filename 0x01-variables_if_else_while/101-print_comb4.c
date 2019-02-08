30ce85f4c846232a2a87a0d0cd135eefdcebafca
Thomas Graeff <676@holbertonschool.com>
2019-02-08 01:44:34 +0000

    Add 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,13 +21,17 @@ int main(void)
 			{
 				if (i < j && j < k)
 				{
-					putchar (i);
-					putchar (j);
-					putchar (k);
+					putchar(i);
+					putchar(j);
+					putchar(k);
 					if (!(i == '7' && j == '8' && k == '9'))
 					{
-						putchar (',');
-						putchar (' ');
+						putchar(',');
+						putchar(' ');
+					}
+					else
+					{
+						putchar('\n');
 					}
 				}
 				k++;
