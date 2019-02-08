298b0df60cae0c32c037d5e3623960b0b6851cd8
Thomas Graeff <676@holbertonschool.com>
2019-02-08 18:11:25 +0000

    Update 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-
+#include <string.h>
+#include <unistd.h>
 /**
  * main - print to standard error
  *
  * Return: return 1
- *
  */
 
 int main(void)
 {
 	char *str = "and that piece of art is useful\" -\
 Dora Korpar, 2015-10-19\n";
-
 	write(2, str, strlen(str));
 
 	return (1);
