af3cf5baf6ec25efffc6d619aab7dd9cafcd3c69
Thomas Graeff <676@holbertonschool.com>
2019-02-13 00:13:25 +0000

    Add 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,7 +4,7 @@
 /**
  * main - compute and print sum of all multiples of 3 or 5
  * below 1024
- * 
+ *
  * Return: 0
  *
  */
@@ -18,7 +18,7 @@ int main(void)
 		y = (x % 3);
 		z = (x % 5);
 		if (y == 0 || z == 0)
-			sum = sum +1;
+			sum = sum + 1;
 	}
 	printf("%d\n", sum);
 	return (0);
