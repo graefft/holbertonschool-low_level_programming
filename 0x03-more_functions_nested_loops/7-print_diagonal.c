2fff526fe2db2ea6b7ce2c5b87aeb37dc4fb0b87
Thomas Graeff <676@holbertonschool.com>
2019-02-14 23:39:05 +0000

    Add 7

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - prints diagonal line on terminal
+ * @n: number of times to print "\"
+ *
+ * Return: void
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int line, space;
+
+	if (n > 0)
+	{
+		for (line = 0; line < n; line++)
+		{
+			for (space = 0; space <= line; space++)
+			{
+				if (space != line)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('\\');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
