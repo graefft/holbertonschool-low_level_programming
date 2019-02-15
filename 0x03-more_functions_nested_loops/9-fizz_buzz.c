5c7f5be49ae7414ae89b77939bf3bfacb0ce9897
Thomas Graeff <676@holbertonschool.com>
2019-02-15 00:12:55 +0000

    Add 9

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints 1 to 100 but
+ * "Fizz" on multiples of 3 and "Buzz on multiples of 5
+ * and FizzBuzz on multiples of 3 and 5
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 != 0 && x % 5 != 0)
+		{
+			printf("%d ", x);
+		}
+		else
+		{
+			if ((x % 3 == 0) && (x % 5 != 0))
+			{
+				printf("Fizz ");
+			}
+			else if ((x % 3 != 0) && (x % 5 == 0))
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("FizzBuzz ");
+			}
+		}
+	}
+	return (0);
+}
