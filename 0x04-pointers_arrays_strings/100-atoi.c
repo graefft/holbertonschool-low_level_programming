a6bda9207d4fefc78c460dddc96df08d2ee55b44
Thomas Graeff <676@holbertonschool.com>
2019-02-19 20:35:45 +0000

    Add 100

---
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to convert
+ *
+ * Return: number
+ */
+
+int _atoi(char *s)
+{
+	int res = 0;
+	int neg = -1;
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			neg *= -1;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			res = res * 10 - (s[i] - '0');
+		}
+		if ((s[i] >= '0' && s[i] <= '9') && (s[i + 1] < '0' || s[i + 1] > '9'))
+			break;
+		i++;
+	}
+	return ( res * neg);
+}
