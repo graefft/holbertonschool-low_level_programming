07bd3cfeb4e7952ae64e7ec164a31702632a6222
Thomas Graeff <676@holbertonschool.com>
2019-02-19 05:29:03 +0000

    Add 9

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copies string pointed to by src
+ * to buffer pointed to by dest
+ *
+ * @dest: points to buffer
+ * @src: points to string
+ *
+ * Return: pointer to the dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+
+	return (dest);
+}
