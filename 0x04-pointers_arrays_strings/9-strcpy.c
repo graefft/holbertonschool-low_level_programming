b8d9494b5a66e7b75a5e89d2bc250526c9d99b2c
Thomas Graeff <676@holbertonschool.com>
2019-02-19 16:49:16 +0000

    Update 9

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -4,7 +4,6 @@
 /**
  * _strcpy - copies string pointed to by src
  * to buffer pointed to by dest
- *
  * @dest: points to buffer
  * @src: points to string
  *
@@ -16,7 +15,9 @@ char *_strcpy(char *dest, char *src)
 	int i;
 
 	for (i = 0; src[i] != '\0'; i++)
+	{
 		dest[i] = src[i];
+	}
 	dest[i] = '\0';
 
 	return (dest);
