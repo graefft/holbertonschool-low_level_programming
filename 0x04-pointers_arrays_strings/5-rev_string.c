3bfaefcf33b4d15b76a23fc57dec7546db687cbe
Thomas Graeff <676@holbertonschool.com>
2019-02-19 03:18:03 +0000

    Add 5

---
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverse a string
+ * @s - string to reverse
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i, j;
+	char x;
+
+	for (i = 0; s[i] != 0; i++)
+	
+	j = 0;
+	i = i -1;
+	while (j < i)
+	{
+		x = s[i];
+		s[i] = s[j];
+		s[j] = x;
+		j++;
+		i--;
+	}
+}
