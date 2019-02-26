03e97e93694ce10b1cccbef04d31cef1a5e9327c
Thomas Graeff <676@holbertonschool.com>
2019-02-26 21:32:53 +0000

    Update 4

--- 0x06-pointers_arrays_strings/4-strpbrk.c
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -15,9 +15,9 @@ char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i]; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
 			{
