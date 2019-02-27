7d6ca12e5218647e0c174d426b417c2ee4a3766e
Thomas Graeff <676@holbertonschool.com>
2019-02-27 22:42:21 +0000

    Add 0

---
+++ 0x07-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - prints a string
+ *
+ * @s: string to print
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar(*s);
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
