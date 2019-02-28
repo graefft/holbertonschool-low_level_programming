bf79351b10afd38f5fb54e10c40bc80a2e0b3dcf
Thomas Graeff <676@holbertonschool.com>
2019-02-28 04:34:46 +0000

    Add 100

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -1,5 +1,18 @@
 #include "holberton.h"
 
+int allstars(char *s2)
+{
+	if (*s2 == '*')
+	{
+		return (allstars(s2 + 1));
+	}
+	else if(*s2 == '\0')
+	{
+		return (1);
+	}
+	return (0);
+}
+
 /**
  * wildcmp - compares two strings and checks if they are identical
  *
@@ -16,7 +29,18 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (1);
 	}
-	if (*s1 == *s2)
+	if (*s1 == '\0')
+	{
+		if (allstars(s2))
+		{
+			return (1);
+		}
+		else 
+		{
+			return (0);
+		}
+	}
+	if (*s1 == *s2 && *s1 != '*')
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
