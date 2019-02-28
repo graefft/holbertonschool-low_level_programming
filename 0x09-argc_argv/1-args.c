eb258d54596fa3af204738120c13d6b600516d96
Thomas Graeff <676@holbertonschool.com>
2019-02-28 20:13:22 +0000

    Add 1

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed into it
+ *
+ * Return: 0
+ *
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%d\n", argc - 1);
+	
+	return (0);
+}
