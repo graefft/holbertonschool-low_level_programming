32d73e3b2ae082180b57c3cae67d3c27b4fd33ba
Thomas Graeff <676@holbertonschool.com>
2019-03-02 19:19:03 +0000

    Add 4

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -1,3 +1,4 @@
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -19,8 +20,7 @@ int main(int argc, char *argv[])
 		sum += atoi(argv[i]);
 		if (!isdigit(*argv[i]))
 		{
-			printf("Error\n");
-			return (1);
+			return (printf("Error\n"), 1);
 		}
 	}
 	printf("%d\n", sum);
