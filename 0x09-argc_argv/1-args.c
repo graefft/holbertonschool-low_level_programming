2bd0f57758d6aa3618cfa453c79fad4cce84d04a
Thomas Graeff <676@holbertonschool.com>
2019-03-02 19:10:28 +0000

    Update 1

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -3,14 +3,15 @@
 
 /**
  * main - prints number of arguments passed into it
- *
+ * @argc: number of arguments
+ * @argv: void
  * Return: 0
- *
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
+	(void)argv;
 	printf("%d\n", argc - 1);
-	
+
 	return (0);
 }
