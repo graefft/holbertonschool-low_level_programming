ee0364f3e478c48d9d16d42cecd75b63dd4e1765
Thomas Graeff <676@holbertonschool.com>
2019-03-05 23:10:39 +0000

    update 0

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -15,17 +15,12 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int x;
 
-	if (size == 0)
-	{
-		return (NULL);
-	}
 	s = malloc(size * sizeof(s));
 
 	if (s == NULL)
 	{
 		return (NULL);
 	}
-
 	for (x = 0; x < size; x++)
 	{
 		s[x] = c;
