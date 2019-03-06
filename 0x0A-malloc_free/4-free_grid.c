3c5f61abd4aee4b0f7121573a01463ff679ea594
Thomas Graeff <676@holbertonschool.com>
2019-03-06 02:28:08 +0000

    Add 4

---
+++ 0x0A-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * alloc_grid - returns pointer to a 2d array of integers
+ * @grid: pointer to grid
+ * @height: height of grid
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+}
