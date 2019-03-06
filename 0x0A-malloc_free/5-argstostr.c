ac8b9edc5bde7dc1fee42f5e5621a23a75ab1a83
Thomas Graeff <676@holbertonschool.com>
2019-03-06 21:11:14 +0000

    update 5

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -11,7 +11,7 @@
 
 char *argstostr(int ac, char **av)
 {
-	char *pts;
+	char *pts, *pts_copy;
 	int i, j, len, x = 0;
 
 	if (ac <= 0 || av == NULL)
@@ -28,18 +28,20 @@ char *argstostr(int ac, char **av)
 	}
 	len++;
 	pts = malloc(len * sizeof(char));
+	pts_copy = pts;
 	if (pts == NULL)
 	{
 		return (NULL);
 	}
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j]; j++; x++)
+		for (j = 0; av[i][j]; j++)
 		{
 			pts[x] = av[i][j];
+			x++;
 		}
 		pts[x++] = '\n';
 	}
 	pts[x] = '\0';
-	return (pts);
+	return (pts_copy);
 }
