d39551460dbcc891aab5aeed3206257a0e3a8c24
Thomas Graeff <676@holbertonschool.com>
2019-03-06 04:47:41 +0000

    update 2

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -10,18 +10,27 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int len1, len2, i = 0;
+	unsigned int len1, len2, i, j = 0;
 	char *s;
 
-	if (s1 == 0)
+	if (s1 == NULL)
 		len1 = 0;
-	if (s2 == 0)
+	else
+	{
+		for (len1 = 0; s1[len1]; len1++)
+			;
+	}
+	if (s2 == NULL)
 		len2 = 0;
-	for (len1 = 0; s1[len1]; len1++)
+	else
+	{
+		for (len2 = 0; s2[len2]; len2++)
 			;
-	for (len2 = 0; s2[len2]; len2++)
-			;
-	s = malloc(sizeof(char) * (len1 + len2 + 1));
+	}
+	j = len1 + len2 + 1;
+	s = malloc(sizeof(char) * j);
+	if (s == NULL)
+		return (NULL);
 	for (i = 0; i < len1; i++)
 	{
 		s[i] = s1[i];
