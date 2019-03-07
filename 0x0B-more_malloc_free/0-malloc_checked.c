0181d2897165da6891023c3089e570bd3c0faabf
Thomas Graeff <676@holbertonschool.com>
2019-03-07 20:46:04 +0000

    Add 0

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -9,15 +9,10 @@
 
 void *malloc_checked(unsigned int b)
 {
-	int *check = malloc(sizeof(int) * b);
+	void *check;
 
+	check = malloc(b);
 	if (check == NULL)
-	{
-		exit (98);
-	}
-	else
-	{
-		return (check);
-		free (check);
-	}
+		exit(98);
+	return (check);
 }
