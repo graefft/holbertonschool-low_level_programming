847f8b5605af130c66768843f87b6a11b464ce90
Thomas Graeff <676@holbertonschool.com>
2019-03-11 23:27:34 +0000

    add free

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -2,28 +2,6 @@
 #include "dog.h"
 
 /**
- * _strcpy - copies string pointed to by src
- * to buffer pointed to by dest
- * @dest: points to buffer
- * @src: points to string
- * Return: pointer to the dest
- */
-
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-
-	return (dest);
-}
-
-/**
  * new_dog - function that creates a new dog
  * @name: name of dog
  * @age: age of dog
