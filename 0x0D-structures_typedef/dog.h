fe6ef145685ca758e5a10ec4d0597d75345495e6
Thomas Graeff <676@holbertonschool.com>
2019-03-11 21:32:24 +0000

    update dog

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -5,14 +5,13 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
- * 
+ *
  * Description: defining a struct for a dog
  */
-struct dog 
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 #endif
