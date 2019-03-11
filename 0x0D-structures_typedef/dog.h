16ff6a30909268bbcbd98093ebc007c7579fe4a0
Thomas Graeff <676@holbertonschool.com>
2019-03-11 21:18:15 +0000

    update dog

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -13,8 +13,4 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *ownder);
-
 #endif /* DOG_H */
