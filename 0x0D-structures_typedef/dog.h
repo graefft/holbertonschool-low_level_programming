ceb898fdbc2a6284f671d87068cac29275007efd
Thomas Graeff <676@holbertonschool.com>
2019-03-11 21:08:10 +0000

    add dog.h

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,19 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
-#define DOG
-
 /**
  * dog - defines a dog
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
  */
-
-struct dog
-{
+struct dog {
 	char *name;
 	float age;
 	char *owner;
-
 };
 #endif
