948b9fb90578c69a2c00412fa745d8bb8203f45b
Thomas Graeff <676@holbertonschool.com>
2019-03-13 22:26:23 +0000

    update 3-main

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -11,16 +11,17 @@
 
 int main(int argc, char *argv[])
 {
+        if (argc != 4)
+        {
+                printf("Error\n");
+                exit(98);
+        }
+
 	int a = atoi(argv[1]);
 	int b = atoi(argv[3]);
 	int x;
 	int (*f)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
 	f = get_op_func(argv[2]);
 	if (!f)
 	{
