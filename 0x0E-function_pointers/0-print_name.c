3f3eb13b91f19ef1df9cfa7b44d0d20525af20c1
Thomas Graeff <676@holbertonschool.com>
2019-03-13 19:49:30 +0000

    update 0

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdio.h>
 
 /**
  * print_name - prints a name
@@ -10,8 +9,5 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	void (*p)(char *);
-
-	p = f;
-	p(name);
+	f(name);
 }
