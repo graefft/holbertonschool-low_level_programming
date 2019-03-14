fd2229d17e86d38b3802aa1c355c3ed5fc7870a9
Thomas Graeff <676@holbertonschool.com>
2019-03-14 00:40:33 +0000

    add 100

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints opcodes of main function
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: 1 if arg not correct, 2 if bytes is negative, else 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, bytes;
+	char *p;
+
+	if (argc != 2)
+	{
+		printf("Error\n"), exit(1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n"), exit(2);
+	}
+	p = (char *)main;
+	for (i = 0; i < (bytes); i++)
+	{
+		printf("%.2hx ", p[i]);
+	}
+	printf("\n");
+	return (0);
+}
