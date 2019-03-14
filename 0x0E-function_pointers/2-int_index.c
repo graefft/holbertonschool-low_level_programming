93df37dd6f787be66722d2a4c0d926214d06152d
Thomas Graeff <676@holbertonschool.com>
2019-03-14 19:29:50 +0000

    optimized 2

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -17,10 +17,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		for (i = 0; i < size; i++)
 		{
-			if (cmp(array[i]));
+			if (cmp(array[i]))
 				return (i);
 		}
 	}
-	else
-		return (-1);
+	return (-1);
 }
