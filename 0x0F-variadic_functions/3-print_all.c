3997e088d5b202a5951db6177edf25d182152b04
Thomas Graeff <676@holbertonschool.com>
2019-03-17 06:29:37 +0000

    update 3

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -1,49 +1,50 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
  * print_char - prints char
- * @pa: char to print
+ * @c: char to print
  * Return: void
  */
 
-void print_char(va_list pa)
+void print_char(va_list c)
 {
-	printf("%c", va_arg(pa, int));
+	printf("%c", va_arg(c, int));
 }
 
 /**
  * print_int - prints int
- * @pa: int to print
+ * @i: int to print
  * Return: void
  */
 
-void print_int(va_list pa)
+void print_int(va_list i)
 {
-	printf("%i", va_arg(pa, int));
+	printf("%i", va_arg(i, int));
 }
 
 /**
  * print_float - prints float
- * @pa: float to print
+ * @f: float to print
  * Return: void
  */
 
-void print_float(va_list pa)
+void print_float(va_list f)
 {
-	printf("%f", va_arg(pa, double));
+	printf("%f", va_arg(f, double));
 }
 
 /**
  * print_string - prints string
- * @pa: string to print
+ * @s: string to print
  * Return: void
  */
 
-void print_string(va_list pa)
+void print_string(va_list s)
 {
 	char *string;
 
-	string = va_arg(pa, char *);
+	string = va_arg(s, char *);
 
 	if (!string)
 		string = "(nil)";
@@ -60,7 +61,7 @@ void print_all(const char * const format, ...)
 {
 	unsigned int i, j;
 
-	print_t va[] = {
+	print_t funcs[] = {
 		{"c", print_char},
 		{"i", print_int},
 		{"f", print_float},
@@ -76,12 +77,12 @@ void print_all(const char * const format, ...)
 	while (format[i] && format)
 	{
 		j = 0;
-		while (va[j].t)
+		while (funcs[j].t)
 		{
-			if (*(va[j].t) == format[i])
+			if (*funcs[j].t == format[i])
 			{
 				printf("%s", sep);
-				va[j].f(pa);
+				funcs[j].f(pa);
 				sep = ", ";
 				break;
 			}
