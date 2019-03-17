2347fb7d1c3f9e1d096bf4f6df574af4b8ff83ec
Thomas Graeff <676@holbertonschool.com>
2019-03-17 05:50:14 +0000

    update header with struct

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -2,6 +2,17 @@
 #define VARIADIC_FUNCTIONS_H
 #include <stdio.h>
 #include <stdarg.h>
+/**
+ * struct printall - print based on type
+ * @type: type to print
+ * @func: function to choose
+ */
+typedef struct printall
+{
+	char *type;
+	void (*f)(va_list);
+} print_type;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
