0d5fd98851e58ff8db268ace3ebd1e899d8050ab
Thomas Graeff <676@holbertonschool.com>
2019-03-18 06:51:36 +0000

    removed excess whitespace

--- 0x0F-variadic_functions/100-hello_holberton.asm
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -8,9 +8,6 @@ main:	            ;tells linker entry point
    mov	eax,4       ;system call number (sys_write)
    int	0x80        ;call kernel
 
-   mov	eax,1       ;system call number (sys_exit)
-   int	0x80        ;call kernel
-
 section	.data
-msg db 'Hello, Holberton', 0xa  ;string to be printed
+msg db 'Hello, Holberton',0xa  ;string to be printed
 len equ $ - msg     ;length of the string
