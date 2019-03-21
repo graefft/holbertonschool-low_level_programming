92fc463be06142c3d5a3b914d7ceb7cac7ca5361
Thomas Graeff <676@holbertonschool.com>
2019-03-21 23:13:20 +0000

    changed if/else statement

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -13,16 +13,11 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (!h->str)
-		{
 			printf("[0] (nil)\n");
-			h = h->next;
-		}
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-			count++;
-		}
+		h = h->next;
+		count++;
 	}
 	return (count);
 }
