5332ddafa1c380fca5851fcf654a702a92c64678
Thomas Graeff <676@holbertonschool.com>
2019-03-21 23:10:04 +0000

    add 0

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: pointer to list_t list
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (!h->str)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+			count++;
+		}
+	}
+	return (count);
+}
