79bb53040c9bd304c0e1171a587b51931dec48c0
Thomas Graeff <676@holbertonschool.com>
2019-03-26 20:56:11 +0000

    update 101

--- 0x12-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,18 +10,14 @@ size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
 
-	/* check if head is NULL */
-	if (!head)
-		exit(98);
-
 	/* transverse through list */
 	while (head)
 	{
-		if (head - head->next >= 0)
+		count++;
+		if (head - head->next > 0)
 		{
 			printf("[%p] %d\n", (void *)head, head->n);
 			head = head->next;
-			count++;
 		}
 		/* if head is NULL */
 		else
