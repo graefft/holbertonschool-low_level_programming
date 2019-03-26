3374d270cf75dee376514d990d29f77ecd714ff0
Thomas Graeff <676@holbertonschool.com>
2019-03-26 18:38:32 +0000

    add 0123

--- 0x12-more_singly_linked_lists/2-add_nodeint.c
+++ 0x12-more_singly_linked_lists/2-add_nodeint.c
@@ -9,18 +9,18 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	// create new node //
+	/* create new node */
 	listint_t *new = malloc(sizeof(listint_t));
 
-	// if failure return NULL //
+	/* if failure return NULL */
 	if (!new)
 		return (NULL);
 
-	// copy data to new node //
+	/* copy data to new node */
 	new->n = n;
-	// copy old pointer to new node pointer //
+	/* copy old pointer to new node pointer */
 	new->next = *head;
-	// point head to new node //
+	/* point head to new node */
 	*head = new;
 
 	return (*head);
--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,13 +12,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new = malloc(sizeof(listint_t));
 	listint_t *prev = *head;
 
-	// check if malloc worked //
+	/* check if malloc worked */
 	if (!new)
 		return (NULL);
 
-	// put data in new node //
+	/* put data in new node */
 	new->n = n;
-	// set new pointer to NULL //
+	/* set new pointer to NULL */
 	new->next = NULL;
 
 	if (*head == NULL)
@@ -27,11 +27,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (new);
 	}
 
-	// transverse through list //
+	/* transverse through list */
 	while (prev->next)
 		prev = prev->next;
 
-	// set last node to point to new node //
+	/* set last node to point to new node */
 	prev->next = new;
 
 	return (new);
