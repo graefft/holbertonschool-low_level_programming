713a20aefa393f2d72920da6b6c79644078b21b5
Thomas Graeff <676@holbertonschool.com>
2019-03-27 18:50:04 +0000

    removed unnecessary lines

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,8 +36,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new->next = temp->next;
 		if (i == idx - 1)
 		{
-			/* new node ptr set to old temp ptr */
-	/*		new->next = temp->next;*/
 			/* temp ptr points to new node */
 			temp->next = new;
 			return (new);
