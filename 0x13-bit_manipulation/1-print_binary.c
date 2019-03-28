5db502e9a1cfe69f0a51497920e637625a164bb9
Thomas Graeff <676@holbertonschool.com>
2019-03-28 18:48:19 +0000

    add 0 & 1

---
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: integer to convert
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	int c, k;
+
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		for (c = 31; c >= 0; c--)
+		{
+			k = n >> c;
+
+			if (k & 1)
+				_putchar('1');
+			else if (k | 0)
+				_putchar('0');
+		}
+	}
+}
