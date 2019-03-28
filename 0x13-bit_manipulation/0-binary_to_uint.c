0e78f296ad887b0dc5aaac6403fff0ea589a2c7d
Thomas Graeff <676@holbertonschool.com>
2019-03-28 19:10:06 +0000

    change NULL case

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -10,9 +10,12 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int total = 0;
 
+	if (!b)
+		return (0);
+
 	while (*b)
 	{
-		if (*b < '0' || *b > '1' || b == NULL)
+		if (*b < '0' || *b > '1')
 			return (0);
 		total *= 2;
 		if (*b++ == '1')
