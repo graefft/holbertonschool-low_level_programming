d2373a8c8819040d48c6398bfa9caeb0cb681acd
Thomas Graeff <676@holbertonschool.com>
2019-03-29 17:39:42 +0000

    changed 63 to sizeof

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -9,10 +9,8 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int input = *n;
-
-	if (index > 63)
+	if (index > sizeof(unsigned long int) * 8 - 1)
 		return (-1);
-	*n = input | (1 << index);
+	*n = *n | (1 << index);
 	return (1);
 }
