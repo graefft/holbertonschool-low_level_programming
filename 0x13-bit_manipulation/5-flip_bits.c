b21566e195d25975dfb2520879f70b8111c8c581
Thomas Graeff <676@holbertonschool.com>
2019-03-29 17:58:20 +0000

    convert back to 2 functions

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -1,21 +1,32 @@
 #include "holberton.h"
 
 /**
- * flip_bits - returns number of bits needed to flip one number to another
+ * count_bits - counts bits in number
+ * @x: number to count
+ * Return: count
+ */
+
+unsigned long int count_bits(unsigned long int x)
+{
+	unsigned long int count = 0;
+
+	while (x)
+	{
+		count += x & 1;
+		x >>= 1;
+	}
+	return (count);
+}
+
+/**
+ * flip_bits - returns number of bits you would need to flip in\
+ * order to get from one number to another
  * @n: first number
  * @m: second number
- * Return: void
+ * Return: number of bits needed to flip
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int diff = n ^ m;
-	unsigned int count = 0;
-
-	while (diff)
-	{
-		count += diff & 1;
-		diff >>= 1;
-	}
-	return (count);
+	return (count_bits(n ^ m));
 }
