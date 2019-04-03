df379879fdab9bc177260b82ea415ec1dd9747aa
Thomas Graeff <676@holbertonschool.com>
2019-04-03 02:22:52 +0000

    changed order

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -12,8 +12,6 @@ int create_file(const char *filename, char *text_content)
 	int fd, fd_wr, length;
 
 	length = 0;
-	while (text_content[length])
-		length++;
 
 	if (!filename)
 		return (-1);
@@ -25,12 +23,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
+		while (text_content[length])
+			length++;
+
 		fd_wr = write(fd, text_content, length);
 		if (fd_wr == -1)
 			return (-1);
 	}
-
 	close(fd);
-
 	return (1);
 }
