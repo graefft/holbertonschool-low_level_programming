657631d46eadf2bd1349f9a0f4490321c67ac360
Thomas Graeff <676@holbertonschool.com>
2019-04-03 04:08:21 +0000

    add 3

---
+++ 0x14-file_io/3-cp.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * main - copie content of file to another file
+ * @argc: number of arguments
+ * @argv: files to add
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int file1, file2, n, cl;
+	char buf[1024];
+
+	if (!(argc == 3))
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
+	file1 = open(argv[1], O_RDONLY);
+	if (file1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	file2 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
+	if (file2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]), exit(99);
+
+	while ((n = read(file1, buf, 1024)) > 0)
+	{
+		if (write(file2, buf, n) != n)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
+
+	cl = close(file1);
+	if (cl < 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cl), exit(100);
+	cl = close(file2);
+	if (cl < 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cl), exit(100);
+	return (0);
+}
