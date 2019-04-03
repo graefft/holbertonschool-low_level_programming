bd2856d89f1143446c1921710942c620ffe0858f
Thomas Graeff <676@holbertonschool.com>
2019-04-03 02:16:34 +0000

    add 1

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of file to create
+ * @text_content: NULL terminated string to write to file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, fd_wr, length;
+
+	length = 0;
+	while (text_content[length])
+		length++;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		fd_wr = write(fd, text_content, length);
+		if (fd_wr == -1)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}
