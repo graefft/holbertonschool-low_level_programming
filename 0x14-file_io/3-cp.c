722f20b0f2fc89bc8cf859f4bdc05c8499795b2a
Thomas Graeff <676@holbertonschool.com>
2019-04-03 04:16:05 +0000

    added if n -1

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 		if (write(file2, buf, n) != n || file2 == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
-	if (file1 == -1)
+	if (n == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
