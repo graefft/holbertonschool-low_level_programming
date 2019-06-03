48fa247694c1b8abe9d85fd5905809ce514172d2
Thomas Graeff <676@holbertonschool.com>
2019-06-03 01:21:08 +0000

    Add 2 3

---
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -0,0 +1,60 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table to add or update key/value for
+ * @key: key. cannot be empty
+ * @value: value associated with the key. must be duplicated, but can be empty
+ * Return: 1 on success, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned int i = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *head = NULL, *new = NULL;
+
+	/* check if no hash table or key */
+	if (!ht || !key)
+		return (0);
+
+	/* set head of table to current index */
+	head = ht->array[i];
+
+	while (head)
+	{
+		head = head->next;
+	}
+
+	/* add new node */
+	new = add_node(key, value);
+	if (!new)
+		return (0);
+	new->next = ht->array[i];
+	ht->array[i] = new;
+	return (1);
+}
+
+/**
+ * add_node - adds new hash node
+ * @key: key of string
+ * @value: value of string
+ * Return: node or NULL if failure
+ */
+hash_node_t *add_node(const char *key, const char *value)
+{
+	hash_node_t *new;
+
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+		return (0);
+
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+
+	new->value = strdup(value);
+
+	return (new);
+}
--- 0x19-hash_tables/hash_tables.h
+++ 0x19-hash_tables/hash_tables.h
@@ -75,11 +75,11 @@ typedef struct shash_table_s
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
-int has_table_set(hash_table_t *ht, const char *key, const char *value);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+hash_node_t *add_node(const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
-
 
 shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
