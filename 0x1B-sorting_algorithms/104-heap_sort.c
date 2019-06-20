c0b84a55ea37fc3e95882e7544ca8a42db026e44
Thomas Graeff <676@holbertonschool.com>
2019-06-20 06:59:33 +0000

    Adds 104

---
+++ 0x1B-sorting_algorithms/104-heap_sort.c
@@ -0,0 +1,65 @@
+#include "sort.h"
+
+/**
+ * heap_sort - sorts an array of integers using Heap Sort
+ * @array: pointer to array to sort
+ * @size: size of array
+ * Return: void
+ */
+void heap_sort(int *array, size_t size)
+{
+	size_t i;
+	
+	for (i = size / 2 - 1; i + 1 > 0; i--)
+		heapify(array, size, i, size);
+
+	for (i = size - 1; i + 1 > 0; i--)
+	{
+		swap(&array[0], &array[i]);
+		if (array[0] != array[i])
+			print_array(array, size);
+		heapify(array, i, 0, size);
+	}
+}
+
+/**
+ * heapify - heapify a subtree rooted with node i which is an index in array
+ * @array: array to sort
+ * @n: size of array
+ * @i: index
+ * Return: void
+ */
+void heapify(int *array, size_t n, int i, size_t size)
+{
+	size_t largest = i;
+	size_t l = (2 * i + 1);
+	size_t r = (2 * i + 2);
+
+	if (l < n && array[l] > array[largest])
+		largest = l;
+
+	if (r < n && array[r] > array[largest])
+		largest = r;
+
+	if ((int)largest != i)
+	{
+		swap(&array[i], &array[largest]);
+		if (array[i] != array[largest])
+			print_array(array, size);
+		heapify(array, n, largest, size);
+	}
+}
+
+/**
+ * swap - swaps two values
+ * @x: pointer to first value
+ * @y: pointer to second value
+ * Return: void
+ */
+void swap(int *x, int *y)
+{
+	int temp = *x;
+
+	*x = *y;
+	*y = temp;
+}
