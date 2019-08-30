c2fd949396605ea793a5bc6b0998f5aa5df75096
Thomas Graeff <676@holbertonschool.com>
2019-08-30 04:17:09 +0000

    Adds iterative binary_search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in a sorted array of integers
+ * using Binary Search algorithm
+ * @array: pointer to first element in array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: index where value is located or -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int i;
+	int m;
+	int l = 0;
+	int r = size - 1;
+
+	if (array == NULL)
+		return (-1);
+
+	while (l <= r)
+	{
+		m = (l + r) / 2;
+		printf("Seaching in array: ");
+		for (i = l; i <= r; i++)
+		{
+			printf("%d", array[i]);
+			if (i != r)
+				printf(", ");
+			else
+				printf("\n");
+		}
+
+		if (array[m] < value)
+			l = m + 1;
+		else if (array[m] > value)
+			r = m - 1;
+		else
+			return (m);
+	}
+	return (-1);
+}
