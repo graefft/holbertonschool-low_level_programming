d4b12561fd5acb1c10c3d159cc6afcefa9f1507d
Thomas Graeff <676@holbertonschool.com>
2019-08-31 16:34:46 +0000

    Combine two functions into one

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -11,43 +11,27 @@
 int exponential_search(int *array, size_t size, int value)
 {
 	size_t i = 1;
+	size_t l, m, r;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	if (array[0] == value)
 		return (0);
 
-	while (i < size && array[i] <= value)
+	while (i < size && array[i] < value)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		i *= 2;
 	}
-	printf("Value found between indexes [%ld] and [%ld]\n", i / 2, i - 1);
-	return (binary_search(array, i, value));
-}
-
-/**
- * binary_search - searches for a value in a sorted array of integers
- * using Binary Search algorithm
- * @array: pointer to first element in array to search in
- * @size: number of elements in array
- * @value: value to search for
- * Return: index where value is located or -1
- */
-int binary_search(int *array, size_t size, int value)
-{
-	size_t i;
-	size_t m;
-	size_t l = size / 2;
-	size_t r = size - 1;
-
-	if (array == NULL)
-		return (-1);
-
+	l = i / 2;
+	if (i >= size)
+		r = size - 1;
+	else
+		r = i;
+	printf("Value found between indexes [%ld] and [%ld]\n", l, r);
 	while (array && l <= r)
 	{
-		m = (r - l) / 2 + l;
 		printf("Searching in array: ");
 		for (i = l; i <= r; i++)
 		{
@@ -57,7 +41,7 @@ int binary_search(int *array, size_t size, int value)
 			else
 				printf("\n");
 		}
-
+		m = (r - l) / 2 + l;
 		if (array[m] < value)
 			l = m + 1;
 		else if (array[m] > value)
