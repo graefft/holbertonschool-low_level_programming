3ab91e7fb41f6829660b910b077ee9560e098821
Thomas Graeff <676@holbertonschool.com>
2019-09-01 04:21:08 +0000

    Adds jump_list function (jump search singly linked list)

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,44 @@
+#include "search_algos.h"
+
+/**
+ * jump_list - searches for a value in sorted list of
+ * integers using Jump Search algorithm
+ * @list: pointer to head of list to search in
+ * @size: number of nodes in list
+ * @value: value to search for
+ * Return: pointer to first node with value, otherwise -1
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	listint_t *prev;
+	size_t jump;
+
+	if (!list)
+		return (NULL);
+
+	prev = list;
+
+	while (list->n < value && list->next)
+	{
+		jump = sqrt(size);
+
+		for (prev = list; jump > 0 && list->next; jump--)
+			list = list->next;
+
+		printf("Value checked at index [%ld] = [%d]\n", list->index,
+														list->n);
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n", prev->index,
+															list->index);
+	list = prev;
+
+	while (list)
+	{
+		printf("Value checked at index[%ld] = [%d]\n", list->index, list->n);
+		if (list->n == value)
+			return (list);
+		list = list->next;
+	}
+	return (NULL);
+}
